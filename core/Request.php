87b40cf6730cc73b92eccb573c499603c68b5846
swircod <dimgit01@gmail.com>
2021-04-02 19:40:07 +0300

    Feat: extending Request functionality

--- core/Request.php
+++ core/Request.php
@@ -14,12 +14,37 @@ class Request
      * @var string
      */
     private static $uri;
+    /**
+     * @var array
+     */
+    private static $post = [];
+    /**
+     * @var array
+     */
+    private static $get = [];
+    /**
+     * @var array
+     */
+    private static $files = [];
+    /**
+     * @var Cookie
+     */
+    private static $cookie;
+    /**
+     * @var Session
+     */
+    private static $session;
 
 
     public static function init()
     {
         self::$httpMethod = $_SERVER['REQUEST_METHOD'];
         self::$uri = rawurldecode($_SERVER['REQUEST_URI']);
+        self::$get = $_GET;
+        self::$post = $_POST;
+        self::$files = $_FILES;
+        self::$cookie = new Cookie();
+        self::$session = new Session();
     }
 
     /**
@@ -53,5 +78,48 @@ class Request
         }
 
         return self::$uri;
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public static function get($name)
+    {
+        return self::$get[$name];
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public static function post($name)
+    {
+        return self::$post[$name];
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public static function files($name)
+    {
+        return self::$files[$name];
+    }
+
+    /**
+     * @return Cookie
+     */
+    public function cookie(): Cookie
+    {
+        return self::$cookie;
+    }
+
+    /**
+     * @return Session
+     */
+    public static function session(): Session
+    {
+        return self::$session;
     }
 }

8c99aa2033ef5c257fd70249d69571429a0754f5
Dima Getman <dima.getman12112001@gmail.com>
2021-06-13 03:38:16 +0300

    Feature: Add UserSeeder
    Create seeder: php vendor/bin/phinx seed:create UserSeeder
    Run seeders: php vendor/bin/phinx seed:run

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $data = [];
+
+        for ($i = 0; $i < 10; $i++) {
+            $email = $faker->email;
+            $password = '123456789';
+
+            $data[] = [
+                'name' => $faker->name,
+                'email' => $email,
+                'email_verified_at' => date('Y-m-d H:i:s'),
+                'password' => password_hash($password, PASSWORD_BCRYPT),
+                'remember_token' => password_hash($email . $password . time(), PASSWORD_BCRYPT),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ];
+        }
+
+        $this->table('users')->insert($data)->save();
+    }
+}
